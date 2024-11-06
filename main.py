4ed95199ac9219cd6a38586094aefdf1a5fd259f
bvnandini <bvnandini19@gmail.com>
2024-11-06 10:43:04 +0530

    updating frontend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
@@ -10,18 +10,23 @@ app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
 class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
+    file = FileField("Choose a file", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     form = UploadFileForm()
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-       # return "File has been uploaded."
+        file = form.file.data  # Get the file
+        filename = secure_filename(file.filename)  # Sanitize the filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  # Save the file
+        return redirect(url_for('uploaded_file', filename=filename))
     return render_template('index.html', form=form)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return f"File '{filename}' has been successfully uploaded."
 
 if __name__ == '__main__':
     app.run(debug=True)
